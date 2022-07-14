21f8d30443f0dd94910ff9336edb38ddfaa08290
khs <kmin135@naver.com>
2022-07-14 22:54:12 +0900

    update, 검색 조회 예제 추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,10 +18,24 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("name");
-            em.persist(member);
+            // insert
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("name");
+//            em.persist(member);
+
+            // 조회 및 업데이트(갱신감지)
+//            Member member = em.find(Member.class, 1L);
+//            System.out.println(member);
+//            member.setName("changeName");
+
+            // 조건있는 조회
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+            System.out.println(resultList);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,12 +2,14 @@ package hellojpa;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Setter @Getter
+@ToString
 public class Member {
     @Id
     private Long id;
