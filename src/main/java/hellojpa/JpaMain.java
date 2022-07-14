ef470c1be4b2082ddec2285ab5b035ae5e171ad7
khs <kmin135@naver.com>
2022-07-14 22:40:55 +0900

    insert 예제

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // 앱당 1개
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 1개 쓰레드 단위
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("name");
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
