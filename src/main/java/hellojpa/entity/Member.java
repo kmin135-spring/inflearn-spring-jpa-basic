46beee4aab22253c5da5a97d3c5adbd87d493f5c
khs <kmin135@naver.com>
2022-07-16 22:53:21 +0900

    MappedSuperclass

--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Entity
 @Setter @Getter
-public class Member {
+public class Member extends BaseEntity {
     // IDENTITY 방식은 DB에 넣어야 PK가 결정되므로 어쩔 수 없이 persist할때 바로 insert가 수행됨 (SEQUENCE 방식과의 차이점)
     // 반면에 시퀀스는 persist 시점에 시퀀스에서 값을 얻어와야함. allocationSize로 튜닝 가능
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
